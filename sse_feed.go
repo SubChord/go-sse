2385f30d8974da552929cb7038faaaf23c151170
Thijs van der Burgt <thijs.van.der.burgt@be-mobile.com>
2021-09-03 11:50:00 +0200

    fix payload parsing

--- sse_feed.go
+++ sse_feed.go
@@ -158,7 +158,7 @@ func (s *SSEFeed) processRaw(b []byte) {
 	}
 
 	payload := strings.TrimRight(string(b), "\n")
-	split := strings.SplitN(payload, ":", 1)
+	split := strings.SplitN(payload, ":", 2)
 
 	// received comment or heartbeat
 	if split[0] == "" {
